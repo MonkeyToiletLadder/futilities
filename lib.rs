106517d5a2618c952c69c45f9a1d68ab96b68151
MonkeyToiletLadder <32585563+MonkeyToiletLadder@users.noreply.github.com>
2021-02-08 13:32:17 -0800

    ELF format

--- lib.rs
+++ lib.rs
@@ -21,6 +21,120 @@ pub trait IsImage {
     fn is_svg(&self) -> Result<bool, quick_xml::Error>;
 }
 
+pub trait IsExecutable {
+    fn is_elf(&self) -> Result<bool, io::Error>;
+}
+
+macro_rules! impl_isexecutable {
+    ($head:ident) => {
+        impl IsExecutable for $head {
+            fn is_elf(&self) -> Result<bool, io::Error> {
+                let mut file = File::open(self)?;
+                let mut bytes: [u8; 24] = [0; 24];
+                file.read_exact(&mut bytes)?;
+                let magic = vec![
+                    0x7F, 
+                    0x45, 
+                    0x4C, 
+                    0x46
+                ];
+                let format: Vec<u8> = (0x01..=0x02).collect();
+                let os: Vec<u8> = (0x00..=0x12).collect();
+                let filetype: Vec<u16> = vec![
+                    0x0000,
+                    0x0100,
+                    0x0200,
+                    0x0300,
+                    0x0400,
+                    0xFE00,
+                    0xFEFF,
+                    0xFF00,
+                    0xFFFF
+                ];
+                let architecture: Vec<u16> = vec![
+                    0x0000,
+                    0x0100,
+                    0x0200,
+                    0x0300,
+                    0x0400,
+                    0x0500,
+                    0x0600,
+                    0x0700,
+                    0x0800,
+                    0x0900,
+                    0x0A00,
+                    0x0B00,
+                    0x0C00,
+                    0x0D00,
+                    0x0E00,
+                    0x0F00,
+                    0x1300,
+                    0x1400,
+                    0x1500,
+                    0x1600,
+                    0x2800,
+                    0x2A00,
+                    0x3200,
+                    0x3E00,
+                    0x8C00,
+                    0xB700,
+                    0xF300,
+                    0x1010
+                ];
+                let pad: Vec<u8> = vec![
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00
+                ];
+                if bytes[0..=3] != magic {
+                    return Ok(false);
+                }
+                // Bitset
+                if let None = format.iter().position(|b| *b == bytes[4]) {
+                    return Ok(false);
+                }
+                // Endian
+                if let None = format.iter().position(|b| *b == bytes[5]) {
+                    return Ok(false);
+                }
+                // Version
+                if bytes[6] != 0x01 {
+                    return Ok(false);
+                }
+                // Os ABI
+                if let None = os.iter().position(|b| *b == bytes[7]) {
+                    return Ok(false);
+                }
+                // 7 bytes of padding
+                if &bytes[9..=15] != pad {
+                    return Ok(false)
+                }
+                // Object file type
+                if let None = filetype.iter().position(|b| *b == ((bytes[16] as u16) << 8) | (bytes[17] as u16)) {
+                    return Ok(false);
+                }
+                // Architecture
+                if let None = architecture.iter().position(|b| *b == ((bytes[18] as u16) << 8) | (bytes[19] as u16)) {
+                    return Ok(false);
+                }
+                // Version
+                if bytes[20] != 0x01 {
+                    return Ok(false);
+                }
+                // 3 bytes of padding
+                if &bytes[21..=23] != [0x00, 0x00, 0x00] {
+                    return Ok(false);
+                }
+                Ok(true)
+            }
+        }
+    }
+}
+
 macro_rules! impl_isimage {
     ($head:ident) => {
         impl IsImage for $head {
@@ -125,3 +239,4 @@ macro_rules! for_each_ident {
 }
 
 for_each_ident!(impl_isimage, Path, String, str);
+for_each_ident!(impl_isexecutable, Path, String, str);
